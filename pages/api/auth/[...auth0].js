8365f2f9f9f6856dd82b63613af954eb92682864
adimar <adimaraisha@gmail.com>
2022-09-23 20:33:55 +0800

    auth_secrete loger

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,11 +1,11 @@
 import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
+console.log("the AUTH0_SECRET env var is set: ", !!process.env.AUTH0_SECRET);
+
 export default handleAuth({
   async login(req, res) {
-    console.log(req);
     await handleLogin(req, res, {
       returnTo: "/home",
     });
   },
 });
-

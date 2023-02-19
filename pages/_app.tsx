072292ac77641ed1c886fdeb549e3ad9193fd6e4
adimar <adimaraisha@gmail.com>
2023-02-19 18:13:36 +0800

    types added on each pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,25 @@
 import { UserProvider } from "@auth0/nextjs-auth0";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NextPage } from "next";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
 import { Provider } from "react-redux";
 import { store } from "../store";
 import "../styles/globals.css";
 
 const queryClient = new QueryClient();
 
-function MyApp({ Component, pageProps }: AppProps) {
-  // const Layout = Component.Layout || ((page) => page);
-  const Layout = Component.getLayout || ((page) => page);
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const Layout = Component.getLayout ?? ((page) => page);
   return (
     <UserProvider>
       <Provider store={store}>
